7c090b3d4a28c55494fe0c421b4adb8baa2eb3d4
E Harrison <65516014+ehrrsn7@users.noreply.github.com>
2023-10-02 08:59:57 -0700

    fixed handleInput formatting

--- server/scripts/handleInput.js
+++ server/scripts/handleInput.js
@@ -1,11 +1,10 @@
 import readline from "readline"
+
 export function handleInput(callback) {
-   readline.emitKeypressEvents(process.stdin)
-   if (process.stdin.isTTY) {
-      process.stdin.setRawMode(true)
-      process.stdin.on('keypress', (_, key) => {
-         callback(key)
-      })
-   }
+   if (!process.stdin.isTTY) return;
+   readline.emitKeypressEvents(process.stdin);
+   process.stdin.setRawMode(true);
+   process.stdin.on('keypress', (_, key) => { callback(key) });
 }
+
 // https://thisdavej.com/making-interactive-node-js-console-apps-that-listen-for-keypress-events/
